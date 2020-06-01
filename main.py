7e64325796042c5b60d4c85acfd482439013d165
ggoldste <gal.goldstein@intel.com>
2020-06-01 17:53:37 +0300

    #started

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from torchtext.vocab import Vocab
+# from torchtext.vocab import Vocab
 from torch.utils.data.dataset import Dataset, TensorDataset
 from pathlib import Path
 from collections import Counter
@@ -12,4 +12,49 @@ import torch.optim as optim
 import random
 from torch.utils.data.dataloader import DataLoader
 
+## from HW1 Wet ##
+from collections import OrderedDict
+import re
+from scipy import optimize
+import pickle
+import scipy.special as special
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import seaborn as sns
+import time
+import os
 
+## Constants ##
+ROOT_TOKEN = "<root>"
+
+def create_dataset_D(file_path):
+    D = list()
+
+    with open(file_path) as f:
+        print("in")
+        sentence, true_tree = [ROOT_TOKEN], list()
+        for line in f:
+            if line == "\n":
+                if true_tree:
+                    D.append((sentence, true_tree))
+                sentence, true_tree = [ROOT_TOKEN], list()
+            else:
+                splited_values = re.split('\t', line)
+                m = splited_values[0]
+                h = splited_values[6]
+                word = splited_values[1]
+                POS = splited_values[3]
+
+                sentence.append(word)
+                true_tree.append((h, m))
+    print(D)
+    return D
+
+def main():
+    D = create_dataset_D(file_path=r'mini_train.labeled')
+
+
+
+if __name__ == "__main__":
+    main()
+    print("g")
