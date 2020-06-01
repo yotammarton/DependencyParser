205a45554641f144ed2abaea2085e8559d0f6b0e
ggoldste <gal.goldstein@intel.com>
2020-06-01 18:36:30 +0300

    # posDataReader

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from torchtext.vocab import Vocab
+from torchtext.vocab import Vocab
 from torch.utils.data.dataset import Dataset, TensorDataset
 from pathlib import Path
 from collections import Counter
@@ -27,34 +27,70 @@ import os
 ## Constants ##
 ROOT_TOKEN = "<root>"
 
-def create_dataset_D(file_path):
-    D = list()
 
-    with open(file_path) as f:
-        print("in")
-        sentence, true_tree = [ROOT_TOKEN], list()
-        for line in f:
-            if line == "\n":
-                if true_tree:
-                    D.append((sentence, true_tree))
-                sentence, true_tree = [ROOT_TOKEN], list()
-            else:
-                splited_values = re.split('\t', line)
-                m = splited_values[0]
-                h = splited_values[6]
-                word = splited_values[1]
-                POS = splited_values[3]
+class PosDataReader:
+    def __init__(self, file, word_dict, pos_dict):  ## call to readData
+        self.file = file
+        self.D = list()
+        self.__readData__()
 
-                sentence.append(word)
-                true_tree.append((h, m))
-    print(D)
-    return D
+        self.word_dict = word_dict  # TODO need it?
+        self.pos_dict = pos_dict  # TODO need it?
+
+    def __readData__(self):
+        """main reader function which also populates the class data structures"""
+
+        with open(self.file) as f:
+            sentence, true_tree = [ROOT_TOKEN], list()
+            for line in f:
+                if line == "\n":
+                    if true_tree:
+                        self.D.append((sentence, true_tree))
+                    sentence, true_tree = [ROOT_TOKEN], list()
+                else:
+                    splited_values = re.split('\t', line)
+                    m = splited_values[0]
+                    h = splited_values[6]
+                    word = splited_values[1]
+                    POS = splited_values[3]
+
+                    sentence.append(word)
+                    true_tree.append((h, m))
+
+    def get_num_sentences(self):
+        """returns num of sentences in data"""
+        return len(self.D)
+
+
+
+# def create_dataset_D(file_path):
+#     D = list()
+#
+#     with open(file_path) as f:
+#         print("in")
+#         sentence, true_tree = [ROOT_TOKEN], list()
+#         for line in f:
+#             if line == "\n":
+#                 if true_tree:
+#                     D.append((sentence, true_tree))
+#                 sentence, true_tree = [ROOT_TOKEN], list()
+#             else:
+#                 splited_values = re.split('\t', line)
+#                 m = splited_values[0]
+#                 h = splited_values[6]
+#                 word = splited_values[1]
+#                 POS = splited_values[3]
+#
+#                 sentence.append(word)
+#                 true_tree.append((h, m))
+#     print(D)
+#     return D
 
 def main():
-    D = create_dataset_D(file_path=r'mini_train.labeled')
-
+    word_dict, pos_dict = dict(), dict()
+    data = PosDataReader(r'mini_train.labeled', word_dict, pos_dict)
+    print(data.D)
 
 
 if __name__ == "__main__":
     main()
-    print("g")
