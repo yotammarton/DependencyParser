4c2ed556fc9d07ab9d5b710607a525d7ddd470e8
ggoldste <gal.goldstein@intel.com>
2020-06-01 20:24:10 +0300

    # add index_dicts

--- main.py
+++ main.py
@@ -24,9 +24,51 @@ import seaborn as sns
 import time
 import os
 
+import eyal.py as eyal
+
 ## Constants ##
 ROOT_TOKEN = "<root>"
 
+def get_vocabs(list_of_paths):
+    """
+        create dictionary with number of appears of each word and each tag
+    """
+    word_dict = defaultdict(int)
+    pos_dict = defaultdict(int)
+
+    for file_path in list_of_paths:
+        with open(file_path) as f:
+            sentence, true_tree = [ROOT_TOKEN], list()
+            for line in f:
+                if line != "\n":
+                    splited_values = re.split('\t', line)
+                    # m = splited_values[0]
+                    # h = splited_values[6]
+                    word = splited_values[1]
+                    pos = splited_values[3]
+
+                    word_dict[word] += 1
+                    pos_dict[pos] += 1
+    return word_dict, pos_dict
+
+def create_idx_dicts(word_dict, pos_dict):
+    """
+    create dictionary with index to each word. also dictionary with index to each pos.
+    we should call this function  only if we create embedding vectors by ourselves.
+    """
+    word_idx_dict = defaultdict(int)
+    pos_idx_dict = defaultdict(int)
+    idx = 0
+    for word in word_dict.keys():
+        word_idx_dict[word] = idx
+        idx += 1
+
+    idx = 0
+    for pos in pos_dict.keys():
+        pos_idx_dict[pos] = idx
+        idx += 1
+
+    return word_idx_dict, pos_idx_dict
 
 class PosDataReader:
     def __init__(self, file, word_dict, pos_dict):  ## call to readData
@@ -52,7 +94,7 @@ class PosDataReader:
                     m = splited_values[0]
                     h = splited_values[6]
                     word = splited_values[1]
-                    POS = splited_values[3]
+                    pos = splited_values[3]
 
                     sentence.append(word)
                     true_tree.append((h, m))
@@ -88,8 +130,14 @@ class PosDataReader:
 
 def main():
     word_dict, pos_dict = dict(), dict()
-    data = PosDataReader(r'mini_train.labeled', word_dict, pos_dict)
-    print(data.D)
+    # dir_path = r'mini_train.labeled'
+    dir_path = r''
+
+    # data = PosDataReader(dir_path, word_dict, pos_dict)
+    # print(data.D)
+
+    PosDataset = eyal.PosDataset(word_dict, pos_dict, dir_path, subset="train1",
+                 padding=False, word_embeddings=None)
 
 
 if __name__ == "__main__":
