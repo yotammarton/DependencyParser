504f4604a956d2d641b444af5417167023ad8c86
Yotam Martin <yotamm@breezometer.com>
2020-06-13 18:59:40 +0300

    ---------- BASIC MODEL PARAMS ----------

--- main.py
+++ main.py
@@ -501,15 +501,15 @@ def plot_graphs(train_accuracy_list, train_loss_list, test_accuracy_list, test_l
 
 def main():
     word_embd_dim = 100  # if using pre-trained choose word_embd_dim from [50, 100, 200, 300]
-    pos_embd_dim = 50
+    pos_embd_dim = 25
     hidden_dim = 125
     MLP_inner_dim = 100
     epochs = 30
     learning_rate = 0.01
     dropout_layers_probability = 0.0
     weight_decay = 0.0
-    alpha = 0.0  # 0.0 means no word dropout
-    min_freq = 10  # minimum term-frequency to include in vocabulary, use 1 if you wish to use all words
+    alpha = 0.25  # 0.0 means no word dropout
+    min_freq = 1  # minimum term-frequency to include in vocabulary, use 1 if you wish to use all words
     use_pre_trained = False
     vectors = f'glove.6B.{word_embd_dim}d' if use_pre_trained else ''
     path_train = "train.labeled"
